70a408d149a6b2136a1c4dd515036c4a0dd6b49f
kf2001 <ennio286@gmail.com>
2021-03-24 21:33:25 +0100

    drag

--- src/views/StickBoard.vue
+++ src/views/StickBoard.vue
@@ -4,8 +4,9 @@ export default {
   components: {},
   data: function () {
     return {
-      maxzindex:1,
+      maxzindex: 1,
       Nota: function (
+        id_,
         testo_,
         posx_,
         posy_,
@@ -14,6 +15,7 @@ export default {
         forecolor_,
         backcolor_
       ) {
+        this.id = id_;
         this.testo = testo_;
         this.posx = posx_;
         this.posy = posy_;
@@ -33,14 +35,14 @@ export default {
     console.log(this.$refs.board);
 
     this.boarddiv.addEventListener("dblclick", this.addNote);
-    this.boarddiv.addEventListener("dragover", this.onDragOver);
+    /*
     this.boarddiv.addEventListener("drop", this.onDrop);
     this.boarddiv.addEventListener("dragover", (event) => {
       event.preventDefault();
-      this.boarddiv.addEventListener("dragenter", (event) => {
-        event.dataTrasfer.dropEffect = "move";
       });
-    });
+     }); this.boarddiv.addEventListener("dragenter", (event) => {
+        event.dataTrasfer.dropEffect = "move";
+      */
   },
   created: function () {
     // this.carica();
@@ -55,23 +57,36 @@ export default {
     carica() {
       if (localStorage.Notes) this.Notes = JSON.parse(localStorage.Notes);
     },
-    onDragOver(evt) {
+  /*   onDragOver(evt) {
       evt.preventDefault();
       // Set the dropEffect to move
       evt.dataTransfer.dropEffect = "move";
     },
-    onDrop(evt) {
-      evt.preventDefault();
+ */
+    onDrop(evt, list) {
+    
+      const itemID = evt.dataTransfer.getData("itemID");
+     
+      let offsets = this.boarddiv.getBoundingClientRect();
+      let top = offsets.top;
+      let left = offsets.left;
+      let note = document.getElementById(itemID);
 
-      console.log("drop");
+      note.style.left = evt.offsetX + left + "px";
+      note.style.top = evt.offsetY + top + "px";
     },
     addNote(evt) {
+      let idmax = 0;
+      if (this.Notes.length)
+        idmax = this.Notes.map((cc) => cc.id).reduce((max, d) =>
+          1*d > 1*max ? 1*d : 1*max
+        );
       let offsets = this.boarddiv.getBoundingClientRect();
       let top = offsets.top;
       let left = offsets.left;
 
       var note = document.createElement("div");
-      note.id = 4321;
+      note.id = 1*idmax + 1;
       note.classList.add("note");
       note.style.left = evt.offsetX + left + "px";
       note.style.top = evt.offsetY + top + "px";
@@ -82,55 +97,45 @@ export default {
       note.style.fontSize = "12px";
       note.style.fontFamily = "Verdana";
       note.style.position = "absolute";
-      note.innerHTML = "ciao";
+      note.innerHTML = note.id;
       note.setAttribute("contenteditable", true);
       note.setAttribute("draggable", true);
-      note.style.zIndex= ++this.maxzindex;
+      note.style.zIndex = ++this.maxzindex;
 
       note.addEventListener("dragstart", function (event) {
-        console.log(88);
-        event.target.classList.add("raised");
+        event.dataTransfer.dropEffect = "move";
         event.dataTransfer.effectAllowed = "move";
+      
+        event.dataTransfer.setData("itemID", this.id);
+     
       });
 
       note.addEventListener("dragend", function (event) {
-        console.log(99);
+       
         event.target.classList.remove("raised");
       });
 
-         note.addEventListener("mousedown", function (event) {
+      note.addEventListener("mousedown", function (event) {
+       
+        event.target.style.zIndex = 12 + Math.floor(Math.random() * 10);
       
-            console.log(event.target.style.zIndex)
-                event.target.style.zIndex=12+Math.floor(Math.random()*10);
-   console.log(event.target.style.zIndex)
-                event.stopPropagation();
-
-      
+        event.stopPropagation();
       });
 
-      /* 
-
-                    css("left", options.posx + "px").
-                    css("top", options.posy + "px").
-                    css("width", options.width + "px").
-                    css("height", options.height + "px").
-                    css("color", options.forecolor).
-                    css("backgroundColor", options.backcolor).
-                    css("fontSize", options.fontsize + "px").
-                    css("fontFamily", options.fontfamily).
-                    attr("contentEditable", "true").
-                    text(options.testo);
- */
-
-      /*   note.keypress(function (e) {
-        if (e.keyCode == 10 || e.keyCode == 13) {
-          e.preventDefault();
-        }
-      }); */
+      let newNota = new this.Nota(
+        note.id,
+        note.innerHTML,
+        evt.offsetX,
+        evt.offsetY,
+        40,
+        60,
+        "black",
+        "yellow"
+      );
+      this.Notes.push(newNota);
 
       this.boarddiv.append(note);
-      //   note.draggable();
-      //     note.focus();
+  
 
       return note;
     },
@@ -150,7 +155,14 @@ export default {
       </div>
     </div>
 
-    <div id="board" ref="board"></div>
+    <div
+      id="board"
+      ref="board"
+      class="drop-zone"
+      @drop="onDrop($event, 1)"
+      @dragover.prevent
+      @dragenter.prevent
+    ></div>
   </div>
 </template>
 
@@ -162,8 +174,8 @@ export default {
   border-radius: 5px;
   float: left;
   z-index: 10;
-  /*    background-color: #474a51; 
-  background-image: url("cork.jpg");*/
+  background-color: #474a51; 
+  background-image: url("cork.jpg");
   background-repeat: repeat;
 }
 
@@ -195,6 +207,5 @@ export default {
   box-shadow: 0.2em 0.2em 0.5em black;
 }
 </style>
-
 
 
