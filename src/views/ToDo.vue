82af6bdd41a43b6ab7e3b31c59c02c129a9d6ec3
kf2001 <ennio286@gmail.com>
2021-04-01 12:48:49 +0200

    todolist

--- src/views/ToDo.vue
+++ src/views/ToDo.vue
@@ -4,14 +4,16 @@ export default {
   components: {},
   data: function () {
     return {
-      value: 1,
       Todos: [],
+      TodosUnDo: [],
+      modo: new Array(100).fill(0),
     };
   },
   mounted: function () {},
   created: function () {
     this.carica();
   },
+  computed: {},
 
   methods: {
     salva() {
@@ -19,6 +21,7 @@ export default {
     },
     carica() {
       if (localStorage.Todos) this.Todos = JSON.parse(localStorage.Todos);
+      this.Todos = this.Todos.sort((a, b) => b.priorita - a.priorita);
     },
     elimina(idx) {
       console.log(idx);
@@ -29,10 +32,28 @@ export default {
       let newTodo = {
         descrizione: "",
         fatto: false,
-        importanza: 0,
+        priorita: 0,
       };
 
       this.Todos.push(newTodo);
+    },
+    edit(e) {
+      this.modo[e] = 1;
+      this.TodosUnDo = JSON.parse(JSON.stringify(this.Todos));
+      this.$forceUpdate();
+    },
+    conferma(e) {
+      console.log(e, 1);
+      this.modo[e] = 0;
+      this.salva();
+      this.carica();
+    },
+    annulla(e) {
+      console.log(e, 2);
+      this.modo[e] = 0;
+
+      this.Todos = JSON.parse(JSON.stringify(this.TodosUnDo));
+      this.Todos = this.Todos.sort((a, b) => b.priorita - a.priorita);
     },
   },
 };
@@ -40,7 +61,7 @@ export default {
 
 <template>
   <div>
-    <el-card style="width: 500px">
+    <el-card style="width: 700px">
       <div slot="header" class="clearfix">
         <el-button
           type="success"
@@ -58,27 +79,65 @@ export default {
         ></el-button>
       </div>
       <table>
-        <th>Descrizione</th>
-        <th>Fatto!</th>
-        <th>Importanza</th>
+        <th style="width: 40%">Descrizione</th>
+
+        <th style="width: 20%">Priorità</th>
+        <th style="width: 10%">Fatto!</th>
+        <th></th>
+        <th></th>
+        <th></th>
         <tr v-for="(td, idx) in Todos" :key="idx">
           <td>
-            <el-input v-model="td.descrizione"></el-input>
+            <el-input :disabled="!modo[idx]" v-model="td.descrizione"></el-input>
+          </td>
+
+          <td>
+            <el-slider
+              :disabled="!modo[idx]"
+              v-model="td.priorita"
+              :step="1"
+              :min="1"
+              :max="10"
+              show-stops
+            >
+            </el-slider>
           </td>
           <td>
-            <el-checkbox v-model="td.fatto"></el-checkbox>
-            <!-- <el-switch v-model="td.fatto" active-text="" inactive-text=""> </el-switch> -->
-          </td>
-          <td>
-            <el-rate v-model="td.importanza"></el-rate>
+            <el-checkbox :disabled="!modo[idx]" v-model="td.fatto"></el-checkbox>
           </td>
           <td>
             <el-button
+              v-show="!modo[idx]"
               type="danger"
               icon="el-icon-delete"
               circle
               size="mini"
               @click="elimina(idx)"
+            ></el-button>
+            <el-button
+              v-show="modo[idx]"
+              type="success"
+              icon="el-icon-check"
+              circle
+              size="mini"
+              @click="conferma(idx)"
+            ></el-button>
+
+            <el-button
+              v-show="!modo[idx]"
+              type="warning"
+              icon="el-icon-edit"
+              circle
+              size="mini"
+              @click="edit(idx)"
+            ></el-button>
+            <el-button
+              v-show="modo[idx]"
+              type="danger"
+              icon="el-icon-error"
+              circle
+              size="mini"
+              @click="annulla(idx)"
             ></el-button>
           </td>
         </tr>
@@ -87,4 +146,8 @@ export default {
   </div>
 </template>
 
-<style lang="css"></style>
+<style lang="css">
+td {
+  padding: 1em;
+}
+</style>
