3f575f5d2aff8e0ccf3b0b2068e841a8c5427c96
kf2001 <ennio286@gmail.com>
2021-04-06 11:33:52 +0200

    workhome

---
+++ src/views/Home.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+  name: "Home",
+  components: {},
+  data: function () {
+    return {
+     
+    };
+  },
+  mounted: function () {},
+  created: function () {
+ 
+  },
+ 
+  methods: {
+   
+  }
+}
+</script>
+
+<template>
+  <div>
+ 
+  </div>
+</template>
+
+<style lang="css">
+
+</style>
